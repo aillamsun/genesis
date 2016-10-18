d99b890823649a328b5251c9667cbb9344cb9edf
sungang <1120_sungang@gmail.com>
2016-10-18 18:02:10 +0800

    add demo

---
+++ genesis-boot-demo/src/main/java/com/flame/boot/demo/Application.java
@@ -0,0 +1,27 @@
+package com.flame.boot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @author liuzh
+ * @since 2015-12-12 18:22
+ */
+@Controller
+@EnableWebMvc
+@SpringBootApplication
+public class Application extends WebMvcConfigurerAdapter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @RequestMapping("/")
+    String home() {
+        return "redirect:countries";
+    }
+}
