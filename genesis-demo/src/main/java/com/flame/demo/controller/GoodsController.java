988fdb3859c8a51900ad34acb520979917804ed5
sungang <1120_sungang@gmail.com>
2016-10-25 11:49:42 +0800

    add log config

--- genesis-demo/src/main/java/com/flame/demo/controller/GoodsController.java
+++ genesis-demo/src/main/java/com/flame/demo/controller/GoodsController.java
@@ -8,6 +8,8 @@ import com.flame.model.Goods;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,6 +24,8 @@ import java.util.List;
 @RequestMapping("/goods")
 public class GoodsController extends BaseController {
 
+    private final Logger log = LoggerFactory.getLogger(GoodsController.class);
+
     @Autowired
     private IGoodsService goodsService;
 
@@ -35,6 +39,7 @@ public class GoodsController extends BaseController {
     @PostMapping
     public BaseResult add(@RequestBody Goods goods) {
         goods.setCreateTime(new Date());
+        log.info(JSON.toJSONString(goods));
         BaseResult result = goodsService.add(goods);
         return result;
     }
@@ -44,19 +49,21 @@ public class GoodsController extends BaseController {
     @GetMapping
     public String getGoods() {
         List<Goods> goodsList = goodsService.getAll();
+        log.info(JSON.toJSONString(goodsList));
         return JSON.toJSONString(goodsList);
     }
 
     @ApiOperation("获取商品明细")
     @GetMapping("/{goods_id}")
-    public String getGoods(@ApiParam("商品ID") @PathVariable("goods_id") Long goods_id) {
+    public String getGoods(@ApiParam(value = "商品ID", required = true) @PathVariable("goods_id") Long goods_id) {
         Goods goods = goodsService.getById(goods_id);
+        log.info(JSON.toJSONString(goods));
         return JSON.toJSONString(goods);
     }
 
     @ApiOperation("删除商品")
     @DeleteMapping("/{goods_id}")
-    public BaseResult delGoods(@ApiParam("商品ID") @PathVariable("goods_id") Long goods_id) {
+    public BaseResult delGoods(@ApiParam(value = "商品ID",  required = true) @PathVariable("goods_id") Long goods_id) {
         BaseResult result = goodsService.deleteById(goods_id);
         return result;
     }
@@ -64,9 +71,9 @@ public class GoodsController extends BaseController {
     @ApiOperation("更新商品")
     @PutMapping("/{goods_id}")
     public BaseResult updateGoods(
-            @ApiParam("商品ID") @PathVariable("goods_id") Long goods_id,
+            @ApiParam(value = "商品ID",  required = true) @PathVariable("goods_id") Long goods_id,
             @ApiParam("商品") @RequestBody Goods goods) {
-        BaseResult result = goodsService.updateById(goods_id,goods);
+        BaseResult result = goodsService.updateById(goods_id, goods);
         return result;
     }
 }
