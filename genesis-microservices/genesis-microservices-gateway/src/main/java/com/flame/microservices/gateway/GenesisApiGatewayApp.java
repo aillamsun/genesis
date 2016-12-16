68bd603edf3b7ce74d0a05ab177f7efacf97c6f3
sungang <1120_sungang@gmail.com>
2016-12-16 17:48:27 +0800

    update

--- genesis-microservices/genesis-microservices-gateway/src/main/java/com/flame/microservices/gateway/GenesisApiGatewayApp.java
+++ genesis-microservices/genesis-microservices-gateway/src/main/java/com/flame/microservices/gateway/GenesisApiGatewayApp.java
@@ -2,6 +2,7 @@ package com.flame.microservices.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
@@ -10,6 +11,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
  * Created by sungang on 2016/10/25.
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableZuulProxy
 public class GenesisApiGatewayApp {
     public static void main(String[] args) {
