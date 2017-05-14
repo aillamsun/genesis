a9c340abb531c3cb7e414ea48987f7d7efaafdd7
孙刚 <1120sungang@gmail.com>
2017-05-14 14:41:21 +0800

    delete code

--- genesis-spring/genesis-spring-plugins/genesis-spring-plugins-mybatis/src/main/java/com/flame/plugin/spring/mybatis/config/BaseMapper.java
+++ genesis-spring/genesis-spring-plugins/genesis-spring-plugins-mybatis/src/main/java/com/flame/plugin/spring/mybatis/config/BaseMapper.java
@@ -1,15 +1,15 @@
-package com.flame.plugin.spring.mybatis.config;
-
-import tk.mybatis.mapper.common.ConditionMapper;
-import tk.mybatis.mapper.common.IdsMapper;
-import tk.mybatis.mapper.common.Mapper;
-import tk.mybatis.mapper.common.MySqlMapper;
-
-/**
- * BaseMapper
- * Created by sungang on 2016/10/21.
- */
-public interface BaseMapper<T> extends Mapper<T>, ConditionMapper,MySqlMapper, IdsMapper {
-    //TODO
-    //FIXME 特别注意，该接口不能被扫描到，否则会出错
-}
+//package com.flame.plugin.spring.mybatis.config;
+//
+//import tk.mybatis.mapper.common.ConditionMapper;
+//import tk.mybatis.mapper.common.IdsMapper;
+//import tk.mybatis.mapper.common.Mapper;
+//import tk.mybatis.mapper.common.MySqlMapper;
+//
+///**
+// * BaseMapper
+// * Created by sungang on 2016/10/21.
+// */
+//public interface BaseMapper<T> extends Mapper<T>, ConditionMapper,MySqlMapper, IdsMapper {
+//    //TODO
+//    //FIXME 特别注意，该接口不能被扫描到，否则会出错
+//}
