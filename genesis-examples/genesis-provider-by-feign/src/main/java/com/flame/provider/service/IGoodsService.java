a9d43d5dc21014fdbfe75bf3694f15d1c8512155
孙刚 <1120sungang@gmail.com>
2017-05-14 16:42:49 +0800

    增加 熔断器Hystrix

--- genesis-examples/genesis-provider-by-feign/src/main/java/com/flame/provider/service/IGoodsService.java
+++ genesis-examples/genesis-provider-by-feign/src/main/java/com/flame/provider/service/IGoodsService.java
@@ -2,20 +2,26 @@ package com.flame.provider.service;
 
 import com.flame.core.response.BaseResult;
 import com.flame.model.Goods;
+import com.google.common.collect.Lists;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.Date;
 import java.util.List;
 
 /**
  * Created by sungang on 2016/10/21.
  */
-@FeignClient(name = "genesis-provider-goods")
+@FeignClient(name = "genesis-provider-goods", fallback = IGoodsService.HystrixClientFallback.class)
 public interface IGoodsService {
 
+    static final Logger LOGGER = LoggerFactory.getLogger(IGoodsService.class);
 
     /**
      * 添加商品
@@ -62,4 +68,41 @@ public interface IGoodsService {
      */
     @RequestMapping(value = "/goods/{goods_id}", method = RequestMethod.PUT)
     BaseResult updateById(@PathVariable("goods_id") Long goods_id, @RequestBody Goods goods);
+
+
+    @ComponentScan
+    static class HystrixClientFallback implements IGoodsService {
+        @Override
+        public BaseResult add(@RequestBody Goods goods) {
+            return null;
+        }
+
+        @Override
+        public List<Goods> getAll() {
+            IGoodsService.LOGGER.info("异常发生，进入 findAll 方法");
+            Goods goods = new Goods();
+            goods.setId(-1l);
+            goods.setGoodsName("defaul good name");
+            goods.setCreateTime(new Date());
+            goods.setStock(-1);
+            List<Goods> goodss = Lists.newArrayList();
+            goodss.add(goods);
+            return goodss;
+        }
+
+        @Override
+        public Goods getById(@PathVariable("goods_id") Long goods_id) {
+            return null;
+        }
+
+        @Override
+        public BaseResult deleteById(@PathVariable("goods_id") Long goods_id) {
+            return null;
+        }
+
+        @Override
+        public BaseResult updateById(@PathVariable("goods_id") Long goods_id, @RequestBody Goods goods) {
+            return null;
+        }
+    }
 }
