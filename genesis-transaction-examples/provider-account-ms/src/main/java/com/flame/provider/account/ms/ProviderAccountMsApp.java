5b09494ebe499f4bf9722d20a4d58b7a47f43081
孙刚 <1120sungang@gmail.com>
2017-05-18 18:04:24 +0800

    增加分布式事务 example

--- genesis-transaction-examples/provider-account-ms/src/main/java/com/flame/provider/account/ms/ProviderAccountMsApp.java
+++ genesis-transaction-examples/provider-account-ms/src/main/java/com/flame/provider/account/ms/ProviderAccountMsApp.java
@@ -1,7 +1,26 @@
 package com.flame.provider.account.ms;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
 /**
  * Created by sungang on 2017/5/18.
  */
-public class ProviderAccountMsApp {
+@SpringBootApplication
+public class ProviderAccountMsApp extends SpringBootServletInitializer {
+
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ProviderAccountMsApp.class);
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(ProviderAccountMsApp.class);
+
+        app.run(args);
+    }
 }
