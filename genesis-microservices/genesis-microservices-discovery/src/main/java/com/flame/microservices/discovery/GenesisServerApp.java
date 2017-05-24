f4abbb474750d928be43c19f8119d292fd7f0f45
孙刚 <1120sungang@gmail.com>
2017-05-24 17:09:32 +0800

    修改 eureka 集群启动方式，

--- genesis-microservices/genesis-microservices-discovery/src/main/java/com/flame/microservices/discovery/GenesisServerApp.java
+++ genesis-microservices/genesis-microservices-discovery/src/main/java/com/flame/microservices/discovery/GenesisServerApp.java
@@ -1,8 +1,12 @@
 package com.flame.microservices.discovery;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by sungang on 2016/10/21.
@@ -18,5 +22,14 @@ public class GenesisServerApp {
     //程序首次启动调用
     public void run(String... strings) throws Exception {
         System.out.println("Genesis Server Boot Successfully");
+    }
+
+
+    @Autowired
+    LoadBalancerClient lb;
+
+    @RequestMapping("/")
+    public ServiceInstance home() {
+        return lb.choose("eureka-server-clustered");
     }
 }
