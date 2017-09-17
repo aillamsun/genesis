230c79bbb67ef5dcb202152898a54a0b975ba43c
孙刚 <1120sungang@gmail.com>
2017-09-17 10:43:54 +0800

    新增分布式事务测试mapper

--- genesis-mapper/src/main/java/com/flame/mapper/UserMoneyMapper.java
+++ genesis-mapper/src/main/java/com/flame/mapper/UserMoneyMapper.java
@@ -1,7 +1,10 @@
 package com.flame.mapper;
 
+import com.flame.core.dao.mybatis.BaseMapper;
+import com.flame.model.TUserMoney;
+
 /**
  * Created by sungang on 2017/9/16.
  */
-public interface UserMoneyMapper {
+public interface UserMoneyMapper extends BaseMapper<TUserMoney> {
 }
