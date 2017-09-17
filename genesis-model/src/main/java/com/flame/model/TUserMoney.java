17344b59bd709abb5145f4989ca0fc5157599341
孙刚 <1120sungang@gmail.com>
2017-09-17 10:44:53 +0800

    新增分布式事务测试model

--- genesis-model/src/main/java/com/flame/model/TUserMoney.java
+++ genesis-model/src/main/java/com/flame/model/TUserMoney.java
@@ -1,7 +1,50 @@
 package com.flame.model;
 
+import com.flame.core.model.BaseModel;
+import tk.mybatis.mapper.annotation.NameStyle;
+import tk.mybatis.mapper.code.Style;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  * Created by sungang on 2017/9/16.
  */
-public class TUserMoney {
+
+@Table(name = "t_user_money")
+@NameStyle(Style.normal)
+public class TUserMoney extends BaseModel{
+
+    @Id
+    private Long id;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    private Double money;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Double getMoney() {
+        return money;
+    }
+
+    public void setMoney(Double money) {
+        this.money = money;
+    }
 }
